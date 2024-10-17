a102e2f885c8df732f7d71319e3fa0cd926742aa
Mihail <mihrdv@gmail.com>
2024-10-17 21:39:19 +0300

    Practiced my first bit of dependency injection

--- FirstSpring/src/main/java/com/example/FirstSpring/student/StudentService.java
+++ FirstSpring/src/main/java/com/example/FirstSpring/student/StudentService.java
@@ -1,11 +1,15 @@
 package com.example.FirstSpring.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Service
 public class StudentService {
 
     public List<Student> studentInfo(){
-        return List.of(new Student(20, "John", "John", LocalDate.of(2004, 6, 10), 1L));
+        return List.of(new Student(20, "John", "John@gmail.com", LocalDate.of(2004, 6, 10), 1L));
     }
 }
