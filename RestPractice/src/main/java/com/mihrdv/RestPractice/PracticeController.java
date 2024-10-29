67d188a1b96cc4ba1d930796fd57f52fc5036017
Mihail <mihrdv@gmail.com>
2024-10-29 08:39:30 +0200

    Practiced using Postman

--- RestPractice/src/main/java/com/mihrdv/RestPractice/PracticeController.java
+++ RestPractice/src/main/java/com/mihrdv/RestPractice/PracticeController.java
@@ -1,13 +1,18 @@
 package com.mihrdv.RestPractice;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PracticeController {
 
-    @GetMapping
+    @GetMapping("/message")
     public String printMessage(){
         return "This is a practice app";
     }
+
+    @PostMapping("/post")
+    public String post(@RequestBody String message){
+        return "Request accepted. Message is : "+ message;
+    }
+
 }
